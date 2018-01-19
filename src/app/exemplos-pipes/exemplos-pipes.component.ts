51e7134a955318aa2602a82cd21da9cde0eb9857
Iarlen <iarlensilva775@gmail.com>
2018-01-18 21:35:15 -0300

    Criar objeto do livro

--- src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExemplosPipesComponent implements OnInit {
 
+  livro: any = {
+    titulo: 'Learning JavaScript Data Structures and Algorithms 2nd ed',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/glqjpRP'
+  };
+
   constructor() { }
 
   ngOnInit() {
