a6a9463139b0d4747d0c9e5ce58a873d92923865
Iarlen <iarlensilva775@gmail.com>
2018-01-19 17:39:47 -0300

    Criando o pipe para deixar a primeira letra da frase em maiúsculo

--- src/app/camel-case.pipe.ts
+++ src/app/camel-case.pipe.ts
@@ -6,7 +6,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CamelCasePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+    let values = value.split(' ');
+    let result = '';
+
+    for (let v of values) {
+      result += this.capitalize(v) + ' ';
+    }
+    return result;
+  }
+
+  capitalize(value: string) {
+    return value.substr(0,1).toUpperCase() +
+           value.substr(1).toLowerCase();
   }
 
 }
