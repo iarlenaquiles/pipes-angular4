e45450fd41ba7132c041f34527e2f485c423490b
Iarlen <iarlensilva775@gmail.com>
2018-01-20 12:35:13 -0300

    Herança do pipe puro

--- src/app/filtro-array-impuro.pipe.ts
+++ src/app/filtro-array-impuro.pipe.ts
@@ -1,12 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { FiltroArrayPipe } from './filtro-array.pipe';
 
 @Pipe({
   name: 'filtroArrayImpuro'
 })
-export class FiltroArrayImpuroPipe implements PipeTransform {
-
-  transform(value: any, args?: any): any {
-    return null;
-  }
+export class FiltroArrayImpuroPipe extends FiltroArrayPipe {
 
 }
