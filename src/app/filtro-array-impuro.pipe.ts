4c2ed58867b78b24a94f39f8630d2a0b9ce0370e
Iarlen <iarlensilva775@gmail.com>
2018-01-20 12:37:06 -0300

    Configurando o pipe impuro

--- src/app/filtro-array-impuro.pipe.ts
+++ src/app/filtro-array-impuro.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { FiltroArrayPipe } from './filtro-array.pipe';
 
 @Pipe({
-  name: 'filtroArrayImpuro'
+  name: 'filtroArrayImpuro',
+  pure: false
 })
 export class FiltroArrayImpuroPipe extends FiltroArrayPipe {
 
