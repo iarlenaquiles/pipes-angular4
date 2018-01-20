5adcdbd05e55025f71a358acb789c9bf15d98d92
Iarlen <iarlensilva775@gmail.com>
2018-01-20 12:24:53 -0300

    Criando o retorno do pipe

--- src/app/filtro-array.pipe.ts
+++ src/app/filtro-array.pipe.ts
@@ -6,7 +6,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FiltroArrayPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+
+    if (value.length === 0 || args === undefined) {
+      return value;
+    }
+
+    let filter = args.toLocaleLowerCase();
+
+    return value.filter(
+      v => v.toLocaleLowerCase().indexOf(filter)
+    );
   }
 
 }
